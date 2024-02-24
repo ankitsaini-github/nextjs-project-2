0e3ddff89bdd8b07b2f31d68bc0d1c04d0230a51
ankitsaini-github <ankit121saini@gmail.com>
2024-02-25 05:18:56 +0530

    added meta data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
 
 export async function getStaticProps() {
-  let meetups=[];
+  let meetups = [];
   // await MongoClient.connect(
   //   "mongodb+srv://ankit:ankit123123123@cluster0.goaussj.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0",
   //   async function (err, client) {
@@ -23,26 +25,36 @@ export async function getStaticProps() {
 
     const db = client.db("meetups");
     meetups = await db.collection("meetups").find({}).toArray();
-    
+
     client.close();
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
 
-
   return {
     props: {
       meetups: meetups.map((meetup) => ({
-                title: meetup.title,
-                address: meetup.address,
-                image: meetup.image,
-                id: meetup._id.toString(),
-              })),
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate:3,
+    revalidate: 3,
   };
 }
 
 export default function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 }
